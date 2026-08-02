dbde31182f42fdc5dc76bd6a7897f04fa155f584
MK VENOM PRO <116007409+mkvenompro@users.noreply.github.com>
2026-08-02 05:21:33 +0300

    Update copyright year in AndroidProducts.mk
    Updated copyright year in AndroidProducts.mk.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,8 @@
 #
-# SPDX-FileCopyrightText: The LineageOS Project
+# Copyright (C) 2025 The LineageOS Project
+#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_spinel.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_spinel-user \
-    lineage_spinel-userdebug \
-    lineage_spinel-eng
