ef4eea61e391088bfc64b0276c22ef057629222b
mkvenompro <mkvenompro@users.noreply.github.com>
2026-08-18 13:49:34 -0700

    AndroidProducts.mk: Add COMMON_LUNCH_CHOICES for LineageOS 23.2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_spinel.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_spinel-trunk_staging-userdebug \
+    lineage_spinel-trunk_staging-user \
+    lineage_spinel-trunk_staging-eng
